040bd5370dd8acd9913aed0d8f5dc7edf07c33ef
BurgheRlyeh <egor6tlou@gmail.com>
2021-10-08 02:26:36 +0700

    commit of revised version 0.5

--- Task_1_1_2/src/main/java/ru/nsu/fit/oop/maximov/SubString.java
+++ Task_1_1_2/src/main/java/ru/nsu/fit/oop/maximov/SubString.java
@@ -1,46 +1,90 @@
 package ru.nsu.fit.oop.maximov;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 
+import static java.lang.Math.max;
+
+
+
 public class SubString {
-    // Boyer-Moore-Horspool SubString Search
-    /*
-    return value
-    -3 - text size smaller than pattern
-    -2 - zero pattern size
-    -1 - no entry of pattern in text
-    otherwise - the first entry
-     */
-    public static int SubStr(String text, String pattern) {
-        if (pattern.length() == 0) {
-            return -2;
+    public static void main(String[] args) {
+//        String text = "GEEKS FOR GEEKS";
+//        String pattern = "GEEK";
+//        search(text, pattern);
+
+        ArrayList<Integer> result = reader("/home/burgherlyeh/Programming/OOP/Task_1_1_2/src/main/java/ru/nsu/fit/oop/maximov/file.txt", "/");
+        for (int i = 0; i < result.size(); ++i) {
+            System.out.println(result.get(i));
         }
-        if (text.length() < pattern.length()) {
-            return -3;
-        }
+    }
 
-        final short CHAR_SIZE = 256;        // size of char in bits
-        int[] shift = new int[CHAR_SIZE];
+    private static ArrayList<Integer> reader(String filepath, String pattern) {
+        int patternLength = pattern.length();
+        ArrayList<Integer> occurs = new ArrayList<>();
 
-        Arrays.fill(shift, pattern.length());
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath), 2 * patternLength)) {
+            char[] buffer = new char[2 * patternLength];
+            reader.read(buffer, 0, 2 * patternLength);
 
-        for (int i = 0; i < pattern.length() - 1; ++i) {
-            shift[pattern.charAt(i)] = pattern.length() - 1 - i;
-        }
+            int bufferNum = 0;
 
-        for (int i = 0;
-             i + pattern.length() <= text.length();
-             i += shift[text.charAt(pattern.length() - 1 + i)])
-        {
-            for (int j = pattern.length() - 1;
-                 text.charAt(i + j) == pattern.charAt(j);)
-            {
-                if (--j < 0) {
-                    return i;
+            do {
+                int shift = bufferNum * patternLength;
+                int occurrence = search(new String(buffer), pattern);
+
+                if (occurrence != -1 &&
+                    (occurs.isEmpty() || occurs.get(occurs.size() - 1) != (shift + occurrence))) {
+                    occurs.add(shift + occurrence);
                 }
+                System.arraycopy(buffer, patternLength, buffer, 0, patternLength);
+                ++bufferNum;
+            } while (reader.read(buffer, patternLength, patternLength) != -1);
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return occurs;
+    }
+
+    public static int search(String text, String pattern) {
+        String concat = pattern + "$" + text;
+        int textLength = concat.length();
+        int patternLength = pattern.length();
+
+        int[] Z = new int[textLength];
+        getZarr(concat.toCharArray(), Z);
+
+        for(int i = 0; i < textLength; ++i){
+            if (Z[i] == patternLength) {
+                return i - patternLength - 1;
             }
         }
 
         return -1;
+    }
+
+    private static void getZarr(char[] str, int[] Z) {
+        int n = str.length;
+        int L = 0, R = 0;
+
+        for(int i = 1; i < n; ++i) {
+            if (i <= R && Z[i - L] < R - i + 1) {
+                Z[i] = Z[i - L];
+                continue;
+            }
+
+            R = max(L = i, R);
+            while(R < n && str[R - L] == str[R]) {
+                ++R;
+            }
+            Z[i] = R-- - L;
+        }
     }
 }
