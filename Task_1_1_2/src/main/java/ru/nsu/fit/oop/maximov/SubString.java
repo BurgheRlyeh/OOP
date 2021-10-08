a69dbc0f110a0ecb6c7fc48721df6ed5fe169e72
BurgheRlyeh <egor6tlou@gmail.com>
2021-10-08 23:20:05 +0700

    Last version

--- Task_1_1_2/src/main/java/ru/nsu/fit/oop/maximov/SubString.java
+++ Task_1_1_2/src/main/java/ru/nsu/fit/oop/maximov/SubString.java
@@ -1,85 +1,99 @@
 package ru.nsu.fit.oop.maximov;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-
 import static java.lang.Math.max;
 
-
-
 public class SubString {
-    public static void main(String[] args) {
-//        String text = "GEEKS FOR GEEKS";
-//        String pattern = "GEEK";
-//        search(text, pattern);
-
-        ArrayList<Integer> result = reader("/home/burgherlyeh/Programming/OOP/Task_1_1_2/src/main/java/ru/nsu/fit/oop/maximov/file.txt", "/");
-        for (int i = 0; i < result.size(); ++i) {
-            System.out.println(result.get(i));
-        }
+    /**
+     * @param filepath filepath
+     * @param pattern pattern to search in file
+     * @return ArrayList of occurrences
+     * @throws IOException
+     */
+    public static ArrayList<Integer> substring(String filepath, String pattern) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filepath), 2 * pattern.length());
+        return substring(reader, pattern);
     }
 
-    private static ArrayList<Integer> reader(String filepath, String pattern) {
-        int patternLength = pattern.length();
-        ArrayList<Integer> occurs = new ArrayList<>();
+    /**
+     * @param reader reader with file
+     * @param pattern pattern to search in file
+     * @return ArrayList of occurrences
+     * @throws IOException
+     */
+    public static ArrayList<Integer> substring(Reader reader, String pattern) throws IOException {
+        int length = pattern.length();                            // length of the pattern
+        ArrayList<Integer> result = new ArrayList<>();           // list with occurrences
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filepath), 2 * patternLength)) {
-            char[] buffer = new char[2 * patternLength];
-            reader.read(buffer, 0, 2 * patternLength);
+        BufferedReader buffReader = new BufferedReader(reader, 2 * length);
 
-            int bufferNum = 0;
-
-            do {
-                int shift = bufferNum * patternLength;
-                int occurrence = search(new String(buffer), pattern);
-
-                if (occurrence != -1 &&
-                    (occurs.isEmpty() || occurs.get(occurs.size() - 1) != (shift + occurrence))) {
-                    occurs.add(shift + occurrence);
-                }
-                System.arraycopy(buffer, patternLength, buffer, 0, patternLength);
-                ++bufferNum;
-            } while (reader.read(buffer, patternLength, patternLength) != -1);
-
-        }
-        catch (IOException e) {
-            e.printStackTrace();
+        char[] buffer = new char[2 * length];                // window with 2 patternLength size
+        // init buffer
+        if (buffReader.read(buffer, 0, 2 * length) == -1) {
+            throw new IOException();
         }
 
-        return occurs;
+        String concat = pattern + "$" + new String(buffer); // concatenated string
+        int[] Z = new int[concat.length()];                 // Z-array of concat's length
+        zArray(concat.toCharArray(), Z, 0);          // init Z-array
+
+        for (int bufferNum = 0;; ++bufferNum) {
+            int shift = bufferNum * length;                 // calculate shift
+            int occur = search(Z, length, concat.length()); // search occurrence
+
+            // if occur exists && (1st occur || single occur)
+            if (occur != -1 &&
+                    (result.isEmpty() || result.get(result.size() - 1) != (shift + occur))) {
+                result.add(shift + occur);                      // add to result list
+            }
+            System.arraycopy(buffer, length, buffer, 0, length);  // shift the buffer by length characters
+
+            if (buffReader.read(buffer, length, length) == -1) {
+                break;
+            }
+
+            concat = pattern + "$" + new String(buffer);    // update concat
+            zArray(concat.toCharArray(), Z, length);       // update Z-array
+        }
+
+        return result;
     }
 
-    public static int search(String text, String pattern) {
-        String concat = pattern + "$" + text;
-        int textLength = concat.length();
-        int patternLength = pattern.length();
-
-        int[] Z = new int[textLength];
-        getZarr(concat.toCharArray(), Z);
-
-        for(int i = 0; i < textLength; ++i){
+    /**
+     * @param Z Z-array
+     * @param patternLength length of pattern to search
+     * @param concatLength length of concatenated string pattern$text
+     * @return index of occurrence
+     */
+    public static int search(int[] Z, int patternLength, int concatLength) {
+        for(int i = 0; i < concatLength; ++i) {
+            // if full match of text and pattern
             if (Z[i] == patternLength) {
-                return i - patternLength - 1;
+                return i - patternLength - 1;   // return occurrence's idx
             }
         }
 
         return -1;
     }
 
-    private static void getZarr(char[] str, int[] Z) {
-        int n = str.length;
-        int L = 0, R = 0;
+    /**
+     * @param str concatenated string pattern$text
+     * @param Z Z-array
+     * @param begin index at which to start updating the z-array
+     */
+    private static void zArray(char[] str, int[] Z, int begin) {
+        int n = str.length;         // length of string
+        int L = 0, R = 0;           // substr = str[L, R - 1]
 
-        for(int i = 1; i < n; ++i) {
+        for(int i = begin; i < n; ++i) {
+            // recalculation based on previous values
             if (i <= R && Z[i - L] < R - i + 1) {
                 Z[i] = Z[i - L];
                 continue;
             }
 
+            // manually calculation
             R = max(L = i, R);
             while(R < n && str[R - L] == str[R]) {
                 ++R;
--- Task_1_1_2/src/test/java/ru/nsu/fit/oop/maximov/SubStringTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/fit/oop/maximov/SubStringTest.java
@@ -1,39 +1,94 @@
 package ru.nsu.fit.oop.maximov;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SubStringTest {
-    @Test   // empty string & empty substring
-    public void substrTest0() {
-        String str = "";
-        String sub = "";
-        int result = -2;
-        assertEquals(SubString.SubStr(str, sub), result);
+    private static Stream<Arguments> providerWithException() throws FileNotFoundException {
+        return Stream.of(
+                // empty file & empty pattern
+                Arguments.of(
+                        new FileReader("EmptyFile.txt"),
+                        "",
+                        IllegalArgumentException.class
+                ),
+                // empty file
+                Arguments.of(
+                        new FileReader("EmptyFile.txt"),
+                        "file",
+                        IOException.class
+                ),
+                // empty pattern
+                Arguments.of(
+                        new FileReader("SmallFile.txt"),
+                        "",
+                        IllegalArgumentException.class
+                )
+        );
     }
 
-    @Test   // empty string
-    public void substrTest1() {
-        String str = "";
-        String sub = "pqr";
-        int result = -3;
-        assertEquals(SubString.SubStr(str, sub), result);
+    @ParameterizedTest
+    @MethodSource("providerWithException")
+    public void substringTestWithExceptions(Reader reader, String pattern, Class exception) {
+        assertThrows(
+                exception,
+                () -> {
+                    SubString.substring(reader, pattern);
+                }
+        );
     }
 
-    @Test   // empty substring
-    public void substrTest2() {
-        String str = "abcdefghijklmnopqrstuvwxyz";
-        String sub = "";
-        int result = -2;
-        assertEquals(SubString.SubStr(str, sub), result);
+    private static Stream<Arguments> provider() throws FileNotFoundException {
+        return Stream.of(
+                // StringReader
+                Arguments.of(
+                        new StringReader("Small file. Not Big file."),
+                        "file",
+                        Arrays.asList(6, 20)
+                ),
+                // pattern = one symbol
+                Arguments.of(
+                        new FileReader("SmallFile.txt"),
+                        "l",
+                        Arrays.asList(3, 4, 8, 22)
+                ),
+                // small file
+                Arguments.of(
+                        new FileReader("SmallFile.txt"),
+                        "file",
+                        Arrays.asList(6, 20)
+                ),
+                // big file
+                Arguments.of(
+                        new FileReader("BigFile.txt"),
+                        "clock will",
+                        Arrays.asList(821, 884, 1109, 1184, 1517, 1563, 1821, 7114)
+                )
+        );
     }
 
-    @Test   // 3 letters from alphabet
-    public void substrTest3() {
-        String str = "abcdefghijklmnopqrstuvwxyz";
-        String sub = "pqr";
-        int result = 15;
-        assertEquals(SubString.SubStr(str, sub), result);
+    @ParameterizedTest
+    @MethodSource("provider")
+    public void findSubStringTest(Reader reader, String pattern, List<Integer> list) throws IOException {
+        ArrayList<Integer> result = SubString.substring(reader, pattern);
+        ArrayList<Integer> answer = new ArrayList<>(list);
+        assertArrayEquals(result.toArray(), answer.toArray());
+    }
+
+    @Test   // big file by filepath
+    public void bigFileByFilepath() throws IOException {
+        ArrayList<Integer> result = SubString.substring("BigFile.txt", "clock will");
+        ArrayList<Integer> answer = new ArrayList<>(Arrays.asList(821, 884, 1109, 1184, 1517, 1563, 1821, 7114));
+        assertArrayEquals(result.toArray(), answer.toArray());
     }
 }
