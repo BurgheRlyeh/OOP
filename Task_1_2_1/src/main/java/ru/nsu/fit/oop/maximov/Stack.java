f120ceafc69267065a10d55ebfdd16b7ac8fe648
BurgheRlyeh <egor6tlou@gmail.com>
2021-10-22 00:42:39 +0700

    Hard deadline version - tests and javadoc added

--- Task_1_2_1/src/main/java/ru/nsu/fit/oop/maximov/Stack.java
+++ Task_1_2_1/src/main/java/ru/nsu/fit/oop/maximov/Stack.java
@@ -1,53 +1,55 @@
 package ru.nsu.fit.oop.maximov;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.EmptyStackException;
 
+/**
+ * My stack realization on dynamically growing array
+ * @param <T> generic type
+ */
 public class Stack<T> {
-    public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(5);
-        stack.push(6);
-        stack.push(7);
-        System.out.println(stack.pop());
-        Stack<Integer> stack789 = new Stack<>();
-        stack789.push(7);
-        stack789.push(8);
-        stack789.push(9);
-        stack.pushStack(stack789);
-        while (stack.count() != 0) {
-            System.out.println(stack.pop());
-        }
-    }
-
     private T[] stack;
     private int size = 0;
-//    private int capacity = 10;
+
+    private static final int DEFAULT_SIZE = 10;
 
     public Stack() {
-        stack = (T[])(new Object[10]);
+        this(DEFAULT_SIZE);
     }
 
+    /**
+     * @param capacity size of stack
+     */
     public Stack(int capacity) {
         stack = (T[])(new Object[capacity]);
     }
 
+    /**
+     * @param elem element to push in stack
+     */
     public void push(T elem) {
         if (stack.length <= size) {
-            stack = Arrays.copyOf(stack, size <<= 1);
+            stack = Arrays.copyOf(stack, size << 1);
         }
         stack[size++] = elem;
     }
 
+    /**
+     * @param src source to push in stack
+     */
     public void pushStack(Stack<T> src) {
-        if (stack.length <= size + src.count()) {
-            stack = Arrays.copyOf(stack, size += src.count() << 1);
+        if (stack.length <= size + src.size()) {
+            stack = Arrays.copyOf(stack, size + src.size() << 1);
         }
-        for (int i = 0; i < src.count(); ++i) {
+        while (src.size() != 0) {
             stack[size++] = src.pop();
         }
     }
 
+    /**
+     * @return last added element in stack
+     */
     public T pop() {
         if (size <= 0) {
             throw new EmptyStackException();
@@ -55,24 +57,31 @@ public class Stack<T> {
         return stack[--size];
     }
 
+    /**
+     * @param num number of elements to pop
+     * @return last num elements in stack
+     */
     public Stack<T> popStack(int num) {
-        if (size < 0 && 0 < num) {
+        if (size <= 0 && 0 < num) {
             throw new EmptyStackException();
         }
         if (num < 0) {
             throw new IllegalArgumentException();
         }
-        if (size <= num) {
+        if (size < num) {
             num = size;
         }
         Stack<T> out = new Stack<T>(num);
-        for (int i = size; num < i; --i) {
+        for (int i = 0; i < num; ++i) {
             out.push(stack[--size]);
         }
         return out;
     }
 
-    public int count() {
+    /**
+     * @return number of elements in stack
+     */
+    public int size() {
         return size;
     }
 }
