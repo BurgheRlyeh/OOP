25871bccc3585a3e04f124227f2fb0fec5d73ea4
BurgheRlyeh <egor6tlou@gmail.com>
2021-12-27 03:59:33 +0700

    Tree - soft deadline version

--- Task_1_3_1/src/main/java/ru/nsu/fit/oop/maximov/tree/Tree.java
+++ Task_1_3_1/src/main/java/ru/nsu/fit/oop/maximov/tree/Tree.java
@@ -1,216 +1,111 @@
 package ru.nsu.fit.oop.maximov.tree;
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
+import java.util.function.Consumer;
 
-import static java.lang.Integer.max;
-
-class Node<T> {
-    public Node<T> left;
-    public T key;
-    public Node<T> right;
-    public Integer height;
-
-    public Node(T data) {
-        this.key = data;
-        height = 1;
-    }
-
-}
-
-public class Tree<E extends Comparable> implements Collection {
-    void preOrder(Node<E> node) {
-        if (node == null) {
-            return;
-        }
-
-        preOrder(node.left);
-        System.out.print(node.key + " ");
-        preOrder(node.right);
-    }
-
+public class Tree<E extends Comparable<E>> implements Collection<E> {
     public static void main(String[] args) {
-        Tree<Integer> tree = new Tree<>();
-
-        /* Constructing tree given in the above figure */
-        tree.root = tree.insert(tree.root, 10);
-        tree.root = tree.insert(tree.root, 20);
-        tree.root = tree.insert(tree.root, 30);
-        tree.root = tree.insert(tree.root, 40);
-        tree.root = tree.insert(tree.root, 50);
-        tree.root = tree.insert(tree.root, 25);
-
-        /* The constructed AVL Tree would be
-             30
-            /  \
-          20   40
-         /  \     \
-        10  25    50
-        */
-        System.out.println("Preorder traversal" +
-                " of constructed tree is : ");
-        tree.preOrder(tree.root);
-
-        tree.root = tree.remove(tree.root, 25);
-        System.out.println("Preorder traversal" +
-                " of constructed tree is : ");
-        tree.preOrder(tree.root);
+        var array = new ArrayList<Integer>();
+        var splitter = array.spliterator();
     }
 
-    Node<E> root;
-    int size;
+    class Node {
+        private Node left;
 
-    Integer height(Node<E> N) {
-        return N == null ? 0 : N.height;
-    }
-    Integer balance(Node<E> N) {
-        return N == null ? 0 : height(N.left) - height(N.right);
-    }
-    void updHeight(Node<E> node) {
-        node.height = 1 + max(height(node.left), height(node.right));
-    }
+        private E key;
+        private Node anc;   // ancestor
+        private int height;
 
-    Node<E> rotateLeft(Node<E> left) {
-        Node<E> right = left.right;
-        Node<E> T2 = right.left;
+        private Node right;
 
-        right.left = left;
-        left.right = T2;
-
-        updHeight(left);
-        updHeight(right);
-
-        return right;
-    }
-    Node<E> rotateRight(Node<E> right) {
-        Node<E> left = right.left;
-        Node<E> T2 = left.right;
-
-        left.right = right;
-        right.left = T2;
-
-        updHeight(left);
-        updHeight(right);
-
-        return left;
-    }
-
-    Node<E> insert(Node<E> node, E key) {
-        // empty tree
-        if (node == null) {
-            return new Node<E>(key);
+        public Node(E data, Node anc) {
+            this.key = data;
+            this.anc = anc;
+            height = 1;
         }
-
-        // less than - insert in left subtree
-        if (key.compareTo(node.key) < 0) {
-            node.left = insert(node.left, key);
-        }
-        // greater than - insert in right subtree
-        else if (0 < key.compareTo(node.key)) {
-            node.right = insert(node.right, key);
-        }
-        // equals to - no duplicates
-        else {
-            return null;
-        }
-
-        // update height
-        updHeight(node);
-
-        if (node.left != null && balance(node) > 1 &&
-                key.compareTo(node.left.key) != 0) {
-            // double rotation (left-right)
-            if (key.compareTo(node.left.key) > 0) {
-                node.left = rotateLeft(node.left);
-            }
-            // simple right rotation
-            return rotateRight(node);
-        }
-        if (node.right != null && balance(node) < -1 &&
-                key.compareTo(node.right.key) != 0) {
-            // double rotation (right-left)
-            if (key.compareTo(node.right.key) < 0) {
-                node.right = rotateRight(node.right);
-            }
-            // simple left rotation
-            return rotateLeft(node);
-        }
-
-        return node;
     }
 
-    Node<E> find(Node<E> node, E key) {
+    private Node root = null;
+    private int size = 0;
+    private boolean isTreeChanged = false;
+
+    private int height(Node node) {
+        return node == null ? -1 : node.height;
+    }
+    private int balance(Node node) {
+        return node == null ? 0 : height(node.right) - height(node.left);
+    }
+    private void updateHeight(Node node) {
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+    }
+    private void updateAncestors(Node x, Node y, Node z) {
+        x.anc = y.anc;
+        y.anc = x;
+        z.anc = y;
+    }
+
+    private Node rotateLeft(Node y) {
+        Node x = y.right;
+        Node z = x.left;
+
+        x.left = y;
+        y.right = z;
+
+        updateAncestors(x, y, z);
+        updateHeight(y);
+        updateHeight(x);
+
+        return x;
+    }
+    private Node rotateRight(Node y) {
+        Node x = y.left;
+        Node z = x.right;
+
+        x.right = y;
+        y.left = z;
+
+        updateAncestors(x, y, z);
+        updateHeight(y);
+        updateHeight(x);
+
+        return x;
+    }
+
+    private Node rebalance(Node node) {
         if (node == null) {
             return null;
         }
 
-        // less than
-        if (key.compareTo(node.key) < 0) {
-            return find(node.left, key);
-        }
-        // greater than
-        else if (key.compareTo(node.key) > 0) {
-            return find(node.right, key);
-        }
-        // equals to
-        else {
-            return node;
-        }
-    }
-
-    Node<E> remove(Node<E> node, E key) {
-        // empty tree
-        if (node == null) {
-            return node;
-        }
-
-        // less than - remove from left subtree
-        if (key.compareTo(node.key) < 0) {
-            node.left = remove(node.left, key);
-        }
-        // greater than - insert in right subtree
-        else if (key.compareTo(node.key) > 0) {
-            node.right = remove(node.right, key);
-        }
-        // equals to
-        else {
-            // 0 or 1 child
-            if (node.left == null || node.right == null) {
-                node = node.left == null ? node.right : node.left;
-            }
-            // 2 children
-            else {
-                node.right = remove(node.right, node.key = find(node, key).key);
-            }
-        }
-
-        // tree - only root
-        if (node == null) {
-            return node;
-        }
-
-        // update height
-        updHeight(node);
-
-        if (balance(node) > 1) {
-            // double rotation (left-right)
-            if (balance(node.left) < 0) {
-                node.left = rotateLeft(node.left);
-            }
-            // simple right rotation
-            return rotateRight(node);
-        }
+        updateHeight(node);
         if (balance(node) < -1) {
-            // double rotation (right-left)
-            if (0 < balance(node.right)) {
+            if (height(node.left.left) <= height(node.left.right)) {
+                node.left = rotateLeft(node.left);
+            }
+            node = rotateRight(node);
+        }
+        else if (balance(node) > 1) {
+            if (height(node.right.right) <= height(node.right.left)) {
                 node.right = rotateRight(node.right);
             }
-            // simple left rotation
-            return rotateLeft(node);
+            node = rotateLeft(node);
         }
 
         return node;
     }
+
+    private Node min(Node node) {
+        if (node == null) {
+            return null;
+        }
+        return node.left != null ? min(node.left) : node;
+    }
+    private Node max(Node node) {
+        if (node == null) {
+            return null;
+        }
+        return node.right != null ? max(node.right) : node;
+    }
+
 
     @Override
     public int size() {
@@ -226,62 +121,259 @@ public class Tree<E extends Comparable> implements Collection {
     public boolean contains(Object o) {
         return find(root, (E) o) != null;
     }
+    private Node find(Node node, E key) {
+        // no such key
+        if (node == null) {
+            return null;
+        }
+
+        // equals to - key found
+        if (key.equals(node.key)) {
+            return node;
+        }
+
+        // less than and greater than
+        return find(key.compareTo(node.key) < 0 ? node.left : node.right, key);
+    }
+
+//    public Spliterator<E> spliterator() {
+//        return new Spliterator<>() {
+//            public static int ORDERED       = 0b10000000;
+//            public static int DISTINCT      = 0b01000000;
+//            public static int SORTED        = 0b00100000;
+//            public static int SIZED         = 0b00010000;
+//            public static int NONNULL       = 0;
+//            public static int IMMUTABLE     = 0;
+//            public static int CONCURRENT    = 0;
+//            public static int SUBSIZED      = 0;
+//
+//            {
+//
+//            }
+//
+//            @Override
+//            public boolean tryAdvance(Consumer<? super E> action) {
+//                return false;
+//            }
+//
+//            @Override
+//            public Spliterator<E> trySplit() {
+//                return null;
+//            }
+//
+//            @Override
+//            public long estimateSize() {
+//                return 0;
+//            }
+//
+//            @Override
+//            public int characteristics() {
+//                return 0;
+//            }
+//        };
+//    }
 
     @Override
-    public Iterator<Node<E>> iterator() {
-        return null;
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private Node node;
+            private final Node last;
+
+            {
+                node = min(root);
+                last = max(root);
+                isTreeChanged = false;
+            }
+
+            private void checkChanges() {
+                if (isTreeChanged) {
+                    throw new RuntimeException("Tree structure was upgraded");
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                checkChanges();
+                return node != last;
+            }
+
+            @Override
+            public E next() {
+                checkChanges();
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+
+                if (node.right != null) {
+                    node = min(node.right);
+                }
+                else if (node != root) {
+                    Node prev = node;
+                    node = node.anc;
+                    if (prev == node.right) {
+                        node = node.anc;
+                    }
+                }
+
+                return node.key;
+            }
+        };
     }
+
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        var array = new Object[size];
+
+        int idx = 0;
+        for (E e : this) {
+            array[idx++] = e;
+        }
+
+        return array;
     }
 
     @Override
-    public boolean add(Object o) {
-        if (insert(root, (E) o) != null) {
-            ++size;
-            return true;
+    public boolean add(E o) {
+        if ((root = insert(root, o, null)) == null) {
+            return false;
         }
-        return false;
+        ++size;
+        return isTreeChanged = true;
+    }
+    private Node insert(Node node, E key, Node anc) {
+        if (node == null) {
+            return new Node(key, anc);
+        }
+
+        // less than - insert in left subtree
+        if (key.compareTo(node.key) < 0) {
+            node.left = insert(node.left, key, node);
+        }
+        // greater than - insert in right subtree
+        else if (0 < key.compareTo(node.key)) {
+            node.right = insert(node.right, key, node);
+        }
+        // equals to - no duplicates
+        else {
+            return null;
+        }
+
+        return rebalance(node);
     }
 
     @Override
     public boolean remove(Object o) {
-        if (this.contains(o)) {
-            remove(root, (E) o);
-            return true;
+        if (!contains(o)) {
+            return false;
         }
-        return false;
+        delete(root, (E) o);
+        --size;
+        return isTreeChanged = true;
+    }
+    private Node delete(Node node, E key) {
+        if (node == null) {
+            return null;
+        }
+
+        // less than - remove from left subtree
+        if (key.compareTo(node.key) < 0) {
+            node.left = delete(node.left, key);
+        }
+        // greater than - insert in right subtree
+        else if (key.compareTo(node.key) > 0) {
+            node.right = delete(node.right, key);
+        }
+        // equals to
+        else {
+            // 0 or 1 child
+            if (node.left == null || node.right == null) {
+                if (node.left != null) {
+                    node.left.anc = node.anc;
+                    node = node.left;
+                }
+                else {
+                    node.right.anc = node.anc;
+                    node = node.right;
+                }
+            }
+            // 2 children
+            else {
+                node.right = delete(node.right, node.key = min(node.right).key);
+            }
+        }
+        node = rebalance(node);
+        return node;
     }
 
     @Override
-    public boolean addAll(Collection c) {
-        return false;
+    public boolean addAll(Collection<? extends E> c) {
+        boolean isChanged = false;
+
+        for (E e : c) {
+            if (add(e)) {
+                isChanged = true;
+            }
+        }
+
+        return isChanged;
     }
 
     @Override
     public void clear() {
-
+        root = null;
+        size = 0;
+        isTreeChanged = false;
     }
 
     @Override
     public boolean retainAll(Collection c) {
-        return false;
+        boolean isChanged = false;
+        for (E e : this) {
+            if (!c.contains(e)) {
+                remove(e);
+                isChanged = true;
+            }
+        }
+        return isChanged;
     }
 
     @Override
     public boolean removeAll(Collection c) {
-        return false;
+        boolean isChanged = false;
+        for (Object obj : c) {
+            if (remove(obj)) {
+                isChanged = true;
+            }
+        }
+        return isChanged;
     }
 
     @Override
     public boolean containsAll(Collection c) {
-        return false;
+        for (Object obj : c) {
+            if (!contains(obj)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
-    public Object[] toArray(Object[] a) {
-        return new Object[0];
+    public <T> T[] toArray(T[] array) {
+        if (array.length < size) {
+            array = Arrays.copyOf(array, size);
+        }
+
+        int idx = 0;
+        for (E e : this) {
+            array[idx++] = (T) e;
+        }
+
+        while (idx < array.length) {
+            array[idx++] = null;
+        }
+
+        return array;
     }
 }
