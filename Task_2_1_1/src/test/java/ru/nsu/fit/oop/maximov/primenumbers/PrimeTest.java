fb6bb6e734baf4b8a0d51f2a01b766028ae31b42
egor6 <egor6tlou@gmail.com>
2022-02-15 17:58:25 +0700

    Thread solution corrected

--- Task_2_1_1/src/test/java/ru/nsu/fit/oop/maximov/primenumbers/PrimeTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/fit/oop/maximov/primenumbers/PrimeTest.java
@@ -1,35 +1,40 @@
 package ru.nsu.fit.oop.maximov.primenumbers;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
 
 public class PrimeTest {
-    static public List<List<Integer>> arrays() {
-        var list = new ArrayList<List<Integer>>();
-
-        list.add(Arrays.asList(6, 8, 7, 13, 9, 4));
-//        list.add(Arrays.asList(6997901, 6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053));
-
-        return list;
+    private static Stream<Arguments> testsProvider() {
+        return Stream.of(
+                Arguments.of(
+                        Arrays.asList(6, 8, 7, 13, 9, 4),
+                        true
+                ),
+                Arguments.of(
+                        Arrays.asList(6997901, 6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053),
+                        false
+                )
+        );
     }
 
     @ParameterizedTest
-    @MethodSource("arrays")
-    void comp(List<Integer> list) {
-        boolean answer = true;
+    @MethodSource("testsProvider")
+    void comp(List<Integer> list, boolean answer) {
         System.out.println("SequentialSolution = " + SequentialSolution(list, answer));
         System.out.println("ThreadSolution = " + ThreadSolution(list, answer));
         System.out.println("ParallelStreamSolution = " + ParallelStreamSolution(list, answer));
     }
 
-    public long SequentialSolution(Collection<Integer> collection, boolean answer) {
+    public long SequentialSolution(List<Integer> list, boolean answer) {
         long time0 = System.currentTimeMillis();
 
         boolean result = false;
-        for (var num : collection) {
+        for (var num : list) {
             if (Prime.isComposite(num)) {
                 result = true;
                 break;
@@ -40,25 +45,43 @@ public class PrimeTest {
         return result == answer ? time - time0 : -1;
     }
 
-    private static final int MAX_THREADS = 6;
+    private static final int MAX_THREADS = 3;
     public long ThreadSolution(List<Integer> list, boolean answer) {
         long time0 = System.currentTimeMillis();
 
-        AtomicBoolean result = new AtomicBoolean(false);
-
-        int chunkSize = (list.size() - 1) / MAX_THREADS + 1;
+        AtomicBoolean result = new AtomicBoolean();
         final ArrayList<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < MAX_THREADS; i++) {
-            int beg = i * chunkSize;
-            int end = Math.min(beg + chunkSize, list.size());
 
-            Thread t = new Thread(() -> result.set(result.get() ||
-                    Prime.isCollectionHasComposite(list.subList(beg, end))));
-            threads.add(t);
-            t.start();
+        int step = Math.max(MAX_THREADS,
+                list.size() / (MAX_THREADS - (list.size() % MAX_THREADS == 0 ? 0 : 1)));
+
+        for (int i = 0; i < list.size(); i += step){
+            int idx = i;
+
+            Thread thread = new Thread() {
+                @Override
+                public void run() {
+                    for (var num : list.subList(idx, idx + Math.min(list.size() - idx, step) - 1)) {
+                        if (isInterrupted()) {
+                            break;
+                        }
+                        if (Prime.isComposite(num)) {
+                            result.set(true);
+                            break;
+                        }
+                    }
+                }
+            };
+            thread.start();
+            threads.add(thread);
         }
 
         for (Thread thread : threads) {
+            if (result.get()) {
+                threads.forEach(Thread::interrupt);
+
+                break;
+            }
             try {
                 thread.join();
             } catch (InterruptedException ignored) {}
@@ -68,14 +91,10 @@ public class PrimeTest {
         return result.get() == answer ? time - time0 : -1;
     }
 
-    public static long ParallelStreamSolution(Collection<Integer> collection, boolean answer) {
+    public static long ParallelStreamSolution(List<Integer> list, boolean answer) {
         long time0 = System.currentTimeMillis();
 
-//        boolean result = Arrays.asList(arr)
-//                .parallelStream()
-//                .anyMatch(Prime::isComposite);
-
-        boolean result = collection
+        boolean result = list
                 .parallelStream()
                 .anyMatch(Prime::isComposite);
 
