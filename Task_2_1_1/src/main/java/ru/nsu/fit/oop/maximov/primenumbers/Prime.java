293564b0ffa23a68d2674fe83d82bcd771e11838
egor6 <egor6tlou@gmail.com>
2022-02-16 15:53:14 +0700

    Everything should work, but it does not

--- Task_2_1_1/src/main/java/ru/nsu/fit/oop/maximov/primenumbers/Prime.java
+++ Task_2_1_1/src/main/java/ru/nsu/fit/oop/maximov/primenumbers/Prime.java
@@ -8,7 +8,7 @@ public class Prime {
             return false;
         }
 
-        for (int i = 2; i * i < n; i++)
+        for (int i = 2; i < n; i++)
             if (n % i == 0)
                 return false;
 
