17c14a54a05b1e9a9be7e24fdafda50d8e23d53e
BurgheRlyeh <egor6tlou@gmail.com>
2022-02-16 20:51:35 +0700

    OMG, I don't even understand the task

--- Task_2_1_1/src/main/java/ru/nsu/fit/oop/maximov/primenumbers/Prime.java
+++ Task_2_1_1/src/main/java/ru/nsu/fit/oop/maximov/primenumbers/Prime.java
@@ -1,20 +1,15 @@
 package ru.nsu.fit.oop.maximov.primenumbers;
 
-import java.util.Collection;
+import java.math.BigInteger;
+import java.util.stream.IntStream;
+
+import static java.lang.Math.sqrt;
 
 public class Prime {
     public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-
-        for (int i = 2; i < n; i++)
-            if (n % i == 0)
-                return false;
-
-        return true;
+        return BigInteger.valueOf(n).isProbablePrime(1);
+//        return 1 < n && IntStream.range(2, (int) sqrt(n) + 1).allMatch(i -> n % i != 0);
     }
-
     public static boolean isComposite(int n) {
         return !isPrime(n);
     }
