326beca30df2817fc014edc91dd35a0d579c6509
egor6 <egor6tlou@gmail.com>
2022-03-01 21:30:53 +0700

    Hard deadline version - result charts added

--- Task_2_1_1/src/test/java/ru/nsu/fit/oop/maximov/composite/CompositeTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/fit/oop/maximov/composite/CompositeTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.RepeatedTest;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -25,7 +26,7 @@ public class CompositeTest {
     @BeforeAll
     static void testDataReader() {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("src/main/java/ru/nsu/fit/oop/maximov/composite/numbers.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("src/test/java/ru/nsu/fit/oop/maximov/composite/data/numbers.txt"));
             for (String text = reader.readLine(); text != null; text = reader.readLine()) {
                 list.add(Integer.parseInt(text));
             }
@@ -34,7 +35,7 @@ public class CompositeTest {
         }
     }
 
-    private static final int TESTS_NUMBER = 100;
+    private static final int TESTS_NUMBER = 10000;
 
     @RepeatedTest(TESTS_NUMBER)
     void test() {
@@ -50,12 +51,22 @@ public class CompositeTest {
         parallelStreamSolutionAverageTime += new ParallelStreamComposite().getTimeExecution(list) / (double) TESTS_NUMBER;
     }
 
+    private static final String format = "#0.00";
+
     @AfterAll
     static void createDiagram() {
-        // just println yet
-        System.out.println(sequentialSolutionAverageTime);
-        System.out.println(threadSolutionAverageTime);
-        System.out.println(threadSolutionAverageTimes);
-        System.out.println(parallelStreamSolutionAverageTime);
+        System.out.println("Average time for sequential solution:\t" +
+                new DecimalFormat(format).format(sequentialSolutionAverageTime));
+
+        System.out.println("Average time for thread solution:\t" +
+                new DecimalFormat(format).format(threadSolutionAverageTime));
+
+        System.out.println("Average time for each of threads:\t");
+        for (var time : threadSolutionAverageTimes) {
+            System.out.print("\t" + new DecimalFormat(format).format(time));
+        }
+
+        System.out.println("\nAverage time for parallel stream solution:" +
+                new DecimalFormat(format).format(parallelStreamSolutionAverageTime));
     }
 }
